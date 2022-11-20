94eec994c601f776822f20acd5c6aff76d7ed8da
Ariel <arielozary@gmail.com>
2022-11-20 19:45:35 +0200

    initial tests

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public bool TestMethod1()
+        {
+            var x = new Payload1(); 
+            return true;
+        }
+    }
+}
